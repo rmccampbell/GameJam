163427950d62c5896e20cfb7d2b3e6ac23a4c2ce
Ryan McCampbell <rmccampbell7@gmail.com>
2016-01-30 22:36:54 -0500

    fixed it

--- fatality.py
+++ fatality.py
@@ -18,6 +18,22 @@ PLAYER2 = 1
 
 SIZE = 100
 
+REDIDLE = None
+
+BLUEDANCE = []
+
+
+def load_images():
+    global REDIDLE, BLUEDANCE
+    REDIDLE = pygame.image.load("redidle.png").convert_alpha()
+    REDIDLE = pygame.transform.scale(REDIDLE, (115, 115))
+    for i in range(1, 9):
+        img = pygame.image.load("bluedance%d.png" % i).convert_alpha()
+        img.set_colorkey((0, 0, 0))
+        img = pygame.transform.scale(img, (115, 115))
+        BLUEDANCE.append(img)
+
+
 class Player:
     def __init__(self):
         self.x = 0
@@ -29,7 +45,7 @@ class Player:
         self.speedy = 0
         self.grounded = True
         self.rect = None
-        self.sprite = None
+        self.sprite = pygame.sprite.Sprite()
         self.index = None
         self.sprite_num = 7
 
@@ -44,6 +60,8 @@ class Game:
         self.win = -1
         self.gravity = .05
 
+        load_images()
+
         self.group = pygame.sprite.Group()
 
         player1 = Player()
@@ -56,10 +74,13 @@ class Game:
         player2.y = HEIGHT*.5
         player2.index = 1
 
+        self.group.add(player1.sprite, player2.sprite)
+
         if player1.power >= player2.power: 
             self.players = [player1, player2]
         else:
             self.players = [player2, player1]
+        self.player1, self.player2 = self.players
 
     def run(self):
         self.running = True
@@ -129,7 +150,6 @@ class Game:
 
     def draw(self, screen):
         screen.fill(BGCOLOR)
-        self.group.empty()
 
         # timer
         clock = str(math.ceil(self.timer/60))
@@ -173,37 +193,27 @@ class Game:
         player1 = self.players[PLAYER1]
         player2 = self.players[PLAYER2]
 
-        player1.sprite = pygame.sprite.Sprite()
-        player1.sprite.image = pygame.image.load("redidle.png").convert()
-        player1.sprite.image.set_colorkey((255, 255, 255))
-        player1.sprite.image = pygame.transform.scale(player1.sprite.image, (115, 115))
-        
+        player1.sprite.image = REDIDLE
+
         if player1.x > player2.x:
             player1.sprite.image = pygame.transform.flip(player1.sprite.image, True, False)
 
         player1.rect = pygame.Rect((player1.x, player1.y), (SIZE, SIZE))
         player1.sprite.rect = player1.rect
 
-        player2.sprite = None
-        player2.sprite = pygame.sprite.Sprite()
-        player2.sprite.image = pygame.image.load("bluedance" + str(player2.sprite_num) + ".png").convert()
+        player2.sprite.image = BLUEDANCE[player2.sprite_num-1]
         
         if (self.timer % 5 == 0):
             player2.sprite_num += 1
             if (player2.sprite_num > 8):
                 player2.sprite_num = 1
 
-        player2.sprite.image.set_colorkey((0, 0, 0))
-        player2.sprite.image = pygame.transform.scale(player2.sprite.image, (115, 115))
-        
         if player2.x > player1.x:
             player2.sprite.image = pygame.transform.flip(player2.sprite.image, True, False)
         
         player2.rect = pygame.Rect((player2.x, player2.y), (SIZE, SIZE))
         player2.sprite.rect = player2.rect
 
-        self.group.add(player1.sprite)
-        self.group.add(player2.sprite)
         self.group.draw(screen)
 
         if not self.win == -1:
